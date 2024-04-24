eff1457eefc4c69dca190e24dbc4dfec1450fe2c
ark948 <armankarimi948@gmail.com>
2024-04-24 15:53:13 +0330

    Updated polls views, urls and templates

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from polls import views
 
+app_name = "polls"
 urlpatterns = [
     # index view /
     path("", views.index, name='index'),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
