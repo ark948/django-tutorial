5c1953942a504cf1a9268df123a4ec9b14735b32
ark948 <armankarimi948@gmail.com>
2024-04-25 22:15:42 +0330

    Added some templates to main

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -6,4 +6,5 @@ app_name = "main"
 urlpatterns = [
     path("", views.index, name="index"),
     path("main/", views.index, name="index"),
+    path("main/login", views.login_view, name="login"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "main/index.html")
+
+def login_view(request):
+    return render(request, "main/login.html")
