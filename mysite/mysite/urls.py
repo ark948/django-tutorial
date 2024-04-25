ce7319790bcae86a97b485784203f76f0957e649
ark948 <armankarimi948@gmail.com>
2024-04-25 19:45:52 +0330

    Added first views to main and template inheritance

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include("main.urls")),
+    path('main/', include("main.urls")),
     path('polls/', include("polls.urls")),
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
